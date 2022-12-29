6bf8e295d93498bea2e36e2a8455a0e049256fe2
Shital Bhanderi <bhanderi.shital.123@gmail.com>
2022-12-29 13:16:56 +0530

    first commit

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,21 +4,23 @@ use Illuminate\Support\Facades\Route;
 use Custome\Auth\Http\Controllers\AuthController;
 use Custome\Auth\Http\Controllers\RegisterController;
 
-Route::get('/login', [AuthController::class, 'show'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'destroy'])
-->name('logout');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/login', [AuthController::class, 'show'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'destroy'])
+    ->name('logout');
 
-Route::get('/register', [RegisterController::class, 'show'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+    Route::get('/register', [RegisterController::class, 'show'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store']);
 
 
 
-Route::middleware([
-    'auth',
-    'verified'
-])->group(function () {
-    Route::get('/home', function () {
-        return view('auth::dashboard');
-    })->name('home');
+    Route::middleware([
+        'auth',
+        'verified'
+    ])->group(function () {
+        Route::get('/home', function () {
+            return view('auth::dashboard');
+        })->name('home');
+    });
 });
