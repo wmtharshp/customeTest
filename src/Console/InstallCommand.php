8208edf4ac75c70100d383a0fc4a81135e5587d1
Shital Bhanderi <bhanderi.shital.123@gmail.com>
2023-01-05 16:05:32 +0530

    uploaded

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -61,8 +61,14 @@ class InstallCommand extends Command
         
         $this->updateKernelFile();
         
-        $this->callSilent('publish:name');
-        
+        $this->callSilent('vendor:publish', ['--tag' => 'datatables', '--force' => true]);
+        $this->callSilent('vendor:publish', ['--provider' => 'Mckenziearts\Notify\LaravelNotifyServiceProvider', '--force' => true]);
+        $this->callSilent('vendor:publish', [ '--provider' => "Spatie\Permission\PermissionServiceProvider", '--force' => true]);
+        $this->callSilent('optimize:clear');
+        $this->callSilent('config:clear');
+        copy(__DIR__.'/../database/2022_12_12_131652_add_google_id_column.php', base_path('database/migrations/2022_12_12_131652_add_google_id_column.php'));
+        copy(__DIR__.'/../database/2023_01_04_073048_add_title_to_users_table.php', base_path('database/migrations/2024_01_04_073048_add_title_to_users_table.php'));
+
         $this->callSilent('migrate');
         $this->callSilent('db:seed', ['--class' => 'PermissionsSeeder', '--force' => true]);
         $this->callSilent('db:seed', ['--class' => 'RoleSeeder', '--force' => true]);
